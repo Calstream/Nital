b4c61be64f2ef54f8731ea54037bc791f73ce069
Attesa <Attesa@DESKTOP-FGCVJT7>
2016-11-21 00:35:16 +0300

    🗿

--- nital1/main.cpp
+++ nital1/main.cpp
@@ -2,6 +2,8 @@
 #include<fstream>
 #include<vector>
 #include<cassert>
+#include<list>
+#include<map>
 using namespace std;
 
 string const iname = "input.txt";
@@ -25,6 +27,23 @@ int find_mismatch(string s1, string s2)
 	return res;
 }
 
+class Graph
+{
+	int Size;
+	map<char,list<char>> Adj;
+	Graph() : Size(0) {}
+	void AddVertex(char v)
+	{
+		Adj.emplace(v,nullptr);
+	}
+
+	void AddEdge(char v1, char v2)
+	{
+		Adj[v1].push_back(v2);
+		Adj[v2].push_back(v1);
+	}
+};
+
 int main()
 {
 	/*vector<string> dict;
@@ -40,14 +59,17 @@ int main()
 	}
 	input.close();*/
 	
-	//assert(find_mismatch("abc", "bbc") == 0);
-	//int reslt = find_mismatch("aac", "aab");
-	//assert(reslt == 2);
-	//assert(find_mismatch("aa", "aaa") == 2);
-	//reslt = find_mismatch("aaaaaaaaa", "aaaaa");
-	//assert(reslt == 5);
-	//assert(find_mismatch("abacus", "abacup") == 5);
-	//assert(find_mismatch("ab", "acc") == 1);
+	
 
 	system("pause");
 }
+
+
+//assert(find_mismatch("abc", "bbc") == 0);
+//int reslt = find_mismatch("aac", "aab");
+//assert(reslt == 2);
+//assert(find_mismatch("aa", "aaa") == 2);
+//reslt = find_mismatch("aaaaaaaaa", "aaaaa");
+//assert(reslt == 5);
+//assert(find_mismatch("abacus", "abacup") == 5);
+//assert(find_mismatch("ab", "acc") == 1);
